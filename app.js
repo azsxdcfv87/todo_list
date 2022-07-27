914a8250ba345394b501d60913a6c90cbce115e3
Allen hsu <chaoyu.hsuu@gmail.com>
2022-07-27 14:27:20 +0800

    feat:add create todo function

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
@@ -22,11 +23,24 @@ db.once('open', () => {
   console.log('Mongoose Connected')
 })
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
   Todo.find() // 取出 Todo model 裡的所有資料
     .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
     .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
     .catch(error => console.error(error)) // 錯誤處理
+})
+
+app.post('/todos', (req, res) => {
+  const name = req.body.name
+  return Todo.create({ name })
+    .then(() => res.redirect('/')) // 新增完成導回首頁
+    .catch(error => console.log(error))
+})
+
+app.get('/todos/new', (req, res) => {
+  return res.render('new')
 })
 
 app.listen(port, () => {
