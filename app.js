534ffffd18d90abc82e1c519f4695275ad548b36
Allen hsu <chaoyu.hsuu@gmail.com>
2022-07-27 11:20:43 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const port = 3000
+
+// 載入 mongoose, 設定連線 mongoDB
+mongoose.connect(process.env.MONGODB_URI)
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('Mongoose error')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('Mongoose Connected')
+})
 
 app.get('/', (req, res) => {
   res.send('Hello Kiki')
