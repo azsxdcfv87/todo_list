4775791322c6a879928ad11ace53f07519152c6e
Allen hsu <chaoyu.hsuu@gmail.com>
2022-07-27 14:07:32 +0800

    feat: modify index.handlebars

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ const exphbs = require('express-handlebars');
 const app = express()
 const port = 3000
 
+const Todo = require('./models/todo') // 載入 Todo model
+
 app.engine('handlebars', exphbs.engine({ defaultLayout: 'main', extname: '.handlebars' }))
 app.set('view engine', 'handlebars')
-app.set('views', './views')
 
 // 載入 mongoose, 設定連線 mongoDB
 mongoose.connect(process.env.MONGODB_URI)
@@ -22,7 +23,10 @@ db.once('open', () => {
 })
 
 app.get('/', (req, res) => {
-  res.send('index')
+  Todo.find() // 取出 Todo model 裡的所有資料
+    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
+    .catch(error => console.error(error)) // 錯誤處理
 })
 
 app.listen(port, () => {
